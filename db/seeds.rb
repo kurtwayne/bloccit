5febed82ec87cdc9cc0792f107d607b0d07eb69f
Kurt Gardner <kurtwgardner@gmail.com>
2016-01-09 15:49:01 -0700

    Lesson 31 Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,9 +21,14 @@ posts = Post.all
   )
 end
 
+# Assign #30 - Assign unique post/comment and checking whether it already exsists
 puts "#{Post.count}"
-Post.find_or_create_by(title: :"The Title", body: "The Body")
+Post.find_or_create_by(title: "The Title", body: "The Body")
 puts "#{Post.count}"
+
+puts "#{Comment.count}"
+Post.find_or_create_by(title: "Test Title", body: "Test Body")
+puts "#{Comment.count}"
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
