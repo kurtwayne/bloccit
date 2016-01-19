2cb6ff0d2f766f09576067044dc8d3949ba2ec58
Kurt Gardner <kurtwgardner@gmail.com>
2016-01-18 18:26:18 -0700

    Lesson 35 Update

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Post, type: :model do
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description:
-    RandomData.random_paragraph) }
-  let(:post) { Post.create!(title: RandomData.random_sentence, body:
-    RandomData.random_paragraph) }
+   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
     it { should belong_to(:topic) }
+
+    it { should validate_presence_of(:title) }
+   it { should validate_presence_of(:body) }
+   it { should validate_presence_of(:topic) }
+
+   it { should validate_length_of(:title).is_at_least(5) }
+   it { should validate_length_of(:body).is_at_least(20) }
+
 # let method, makes the Post instance available through the rest of the spc, so we only need to instantiate it
   describe "attributes" do
 # We test whether post has an attributed name title -- we want a non-nil value when called.
