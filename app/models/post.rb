b3c1ee89804180ca50fd320da6b6f08100ceac92
Kurt Gardner <kurtwgardner@gmail.com>
2016-01-30 14:14:11 -0700

    Assignment 43 Update

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ActiveRecord::Base
    has_many :labels, through: :labelings
    has_many :ratings
 
+   after_create :create_vote
+
    default_scope { order('rank DESC') }
    scope :ordered_by_title, -> { order(title: :asc) }
    scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
@@ -32,5 +34,11 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+
+   private
+
+   def create_vote
+     user.votes.create(value: 1, post: self)
    end
  end
