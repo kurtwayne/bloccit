ac10350049016b632b4e9aaaba096c18755ab43e
Kurt Gardner <kurtwgardner@gmail.com>
2016-02-13 14:31:47 -0700

    Lesson 49 Update

--- config/routes.rb
+++ config/routes.rb
@@ -48,8 +48,8 @@ end
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show]
-      resources :topics, only: [:index, :show]
+      resources :users, only: [:index, :show, :create, :update]
+      resources :topics, except: [:edit, :new]
       resources :posts, only: [:index, :show]
       resources :comments, only: [:index, :show]
     end
